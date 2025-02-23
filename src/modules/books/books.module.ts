2c437df71a3e89960bae8c37cb684a157acd2b6c
nam.thanh <tranthanhnam@outlook.com>
2025-02-23 11:17:13 +0700

    fix: validate Dto

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksResolver } from './books.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../../database/entities/user.entity';
+import { BookEntity } from '../../database/entities/book.entity';
+import { UserRepository } from '../../database/repositories/user.repository';
+import { BookRepository } from '../../database/repositories/book.repository';
 
 @Module({
-  providers: [BooksResolver, BooksService],
+  imports: [TypeOrmModule.forFeature([UserEntity, BookEntity])],
+  providers: [BooksResolver, BooksService, UserRepository, BookRepository],
 })
 export class BooksModule {}
--- src/modules/books/dto/update-book.input.ts
+++ src/modules/books/dto/update-book.input.ts
@@ -1,6 +1,9 @@
 import { CreateBookInput } from './create-book.input';
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class UpdateBookInput extends PartialType(CreateBookInput) {
+  @IsNumber({}, { message: 'id must be a number' })
+  @IsNotEmpty({ message: 'id is required' })
   id: number;
 }
